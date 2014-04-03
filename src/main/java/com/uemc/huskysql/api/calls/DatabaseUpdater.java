088c86f798d06dcb2c578fe81743147d4faf9a95
Rockett8855 <rockett8855@gmail.com>
2014-04-03 00:19:00 -0700

    Make the update method throw and SQLException to allow the user to handler it

--- src/main/java/com/uemc/huskysql/api/calls/DatabaseUpdater.java
+++ src/main/java/com/uemc/huskysql/api/calls/DatabaseUpdater.java
@@ -1,5 +1,7 @@
 package com.uemc.huskysql.api.calls;
 
+import java.sql.SQLException;
+
 /**
  * Updater interface used for updating some data in a database, this class should be used mostly for <code>UPDATE</code> SQL statements
  */
@@ -7,7 +9,9 @@ public interface DatabaseUpdater extends DatabaseCall, Runnable {
 
     /**
      * Updates the database with some information, shouldn't retrieve any data, just update
+     * 
+     * @throws SQLException if an error occurs while the method is interacting with the database
      */
-    void update();
+    void update() throws SQLException;
     
 }
